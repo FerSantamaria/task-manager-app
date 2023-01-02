e234310630dd404fb005476c8fb0ba1311b990ff
Fer Santamaria <fer.santamaria@hotmail.com>
2023-01-02 01:09:31 -0600

    Horizontal scroll

--- src/components/styled/pages/Dashboard.styled.js
+++ src/components/styled/pages/Dashboard.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
+import ScrollContainer from 'react-indiana-drag-scroll'
 
-export const StyledColumnsContainer = styled.div`
+export const StyledColumnsContainer = styled(ScrollContainer)`
   padding-top: 16px;
   padding-right: 32px;
   width: 100%;
